b4272530346ff9789235cb623ca93abc473ba8cc
Daniel Papanek <papanek82@gmail.com>
2017-11-12 23:49:10 -0800

    Created first knockout ajax call for skills

---
+++ dpapanek/ViewModels/dpapanek-viewmodel.js
@@ -0,0 +1,13 @@
+﻿function dpapanekViewModel() {
+    var self = this;
+    self.skill_category = ko.observableArray([]);
+
+    $(document).ready(function () {
+        $.get('/api/skill_category', function (skillx) {
+            console.log(skillx);
+            self.skill_category(skillx);
+        });
+    });
+}
+
+ko.applyBindings(new dpapanekViewModel());
